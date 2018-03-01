38e58c500c74f5fbfd78dbd76836ca6957ea7800
TomerMeirman <tmeirman@gmail.com>
2018-03-01 12:41:51 +0200

    Add files via upload

--- navigation.py
+++ navigation.py
@@ -19,3 +19,46 @@ def calc_vector(p1, p2):
     norm = length((p2.x - p1.x, p2.y - p1.y))
     direction = ((p2.x - p1.x) / norm, (p2.y - p1.y) / norm)
     return distance, direction
+
+
+def line_magnitued(x1, y1, x2, y2):
+    lineMagnitude = math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
+    return lineMagnitude
+
+
+# Calc minimum distance from a point and a line segment (i.e. consecutive vertices in a polyline).
+def distance_point_line(px, py, x1, y1, x2, y2):
+    # http://local.wasp.uwa.edu.au/~pbourke/geometry/pointline/source.vba
+    line_mag = line_magnitued(x1, y1, x2, y2)
+
+    if line_mag < 0.00000001:
+        distance = 9999
+        return distance
+
+    u1 = (((px - x1) * (x2 - x1)) + ((py - y1) * (y2 - y1)))
+    u = u1 / (line_mag * line_mag)
+
+    if (u < 0.00001) or (u > 1):
+        # // closest point does not fall within the line segment, take the shorter distance
+        # // to an endpoint
+        ix = line_magnitued(px, py, x1, y1)
+        iy = line_magnitued(px, py, x2, y2)
+        if ix > iy:
+            distance = iy
+        else:
+            distance = ix
+    else:
+        # Intersecting point is on the line, use the formula
+        ix = x1 + u * (x2 - x1)
+        iy = y1 + u * (y2 - y1)
+        distance = line_magnitued(px, py, ix, iy)
+
+    return distance
+
+#p1,p2,c - Positions, r - circle radius
+def intersects_circle(p1,p2,c,r=10):
+    d = distance_point_line(c.x, c.y, p1.x, p1.y, p2.x, p2.y)
+    if d <= r:
+        return True
+    else:
+        return False
